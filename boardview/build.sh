ac39b9945873807db7484ab6b3bb0c0dd337bf6c
Grif <gpolonus@gmail.com>
2025-06-16 10:30:32 -0500

    Fixed needing to move the package jsons around

--- boardview/build.sh
+++ boardview/build.sh
@@ -2,5 +2,5 @@ rm -rf dist
 npx vite build
 mkdir dist/images
 cp images/* dist/images
-mkdir dist/assets/utils
-cp utils/jquery.js dist/assets/utils
+mkdir dist/utils
+cp utils/jquery.js dist/utils
