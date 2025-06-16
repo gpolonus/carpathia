70aa222680c041403e333a12fc160797e7c0a139
Grif <gpolonus@gmail.com>
2025-06-16 13:44:17 -0500

    Changing the path for the boardview

--- boardview/build.sh
+++ boardview/build.sh
@@ -1,6 +1,9 @@
-rm -rf dist
+rm -rf dist build
 npx vite build
 mkdir dist/images
 cp images/* dist/images
 mkdir dist/utils
 cp utils/jquery.js dist/utils
+
+mkdir -p build/carpathia/boardview
+mv dist/* build/carpathia/boardview
